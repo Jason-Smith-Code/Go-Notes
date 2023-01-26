709c126cd61805a7d0dc5eacfa8583d21c8cecf8
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2023-01-26 09:39:15 +0000

    Update functions.go

--- learning/notes/functions.go
+++ learning/notes/functions.go
@@ -1,1 +1,19 @@
 package main
+
+import "fmt"
+
+// creating a function : func functionName(arguments) {}
+
+// func main() : the entry point of the application
+
+func main() {
+	// invoke functions
+	port := 3000
+	port, err := startWebServer(port)
+	fmt.Println(err)
+}
+
+func startWebServer(port int) (int, error) { // int + error refers to the data type returned by the function
+	// return data
+	return port, nil
+}
