76c5f8f4c78dc5f46470a781f794a2f2e6519425
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2023-01-27 14:23:27 +0000

    Update operators.go

--- learning/notes/operators.go
+++ learning/notes/operators.go
@@ -1,6 +1,20 @@
 package main
 
 /*
+Arithmetic Operators
+
++	Addition	Adds together two values	x + y
+-	Subtraction	Subtracts one value from another	x - y
+*	Multiplication	Multiplies two values	x * y
+/	Division	Divides one value by another	x / y
+%	Modulus	Returns the division remainder	x % y
+++	Increment	Increases the value of a variable by 1	x++
+--	Decrement	Decreases the value of a variable by 1
+*/
+
+/*
+Assignment Operators
+
 +	Addition	Adds together two values	x + y
 -	Subtraction	Subtracts one value from another	x - y
 *	Multiplication	Multiplies two values	x * y
